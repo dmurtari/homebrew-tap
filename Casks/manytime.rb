bf3365c9aa0e5b0551c3445c19d745f57e7bf8bb
Domenic Murtari <domenic@murtari.me>
2026-08-11 11:15:12 +0900

    Fix tap linting and add homepage stanza

--- Casks/manytime.rb
+++ Casks/manytime.rb
@@ -4,16 +4,18 @@ cask "manytime" do
 
   url "https://codeberg.org/dmurtari/ManyTime/releases/download/#{version}/ManyTime-#{version}.zip"
   name "ManyTime"
+  homepage "https://codeberg.org/dmurtari/ManyTime"
+
+  depends_on :macos
+
   app "ManyTime.app"
 
   postflight do
     system_command "/usr/bin/xattr",
-                  args: ["-dr", "com.apple.quarantine", "#{appdir}/ManyTime.app"]
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/ManyTime.app"]
     system_command "/usr/bin/codesign",
-                  args: ["--force", "--deep", "--sign", "-", "#{appdir}/ManyTime.app"]
+                   args: ["--force", "--deep", "--sign", "-", "#{appdir}/ManyTime.app"]
   end
 
-  zap trash: [
-    "~/Library/Preferences/com.dmurtari.ManyTime.plist",
-  ]
+  zap trash: "~/Library/Preferences/com.dmurtari.ManyTime.plist"
 end
